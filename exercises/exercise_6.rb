e5afd0844e69c1b029af9aa9d838ad5190cfea7b
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-01-04 03:46:30 -0500

    solve exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,16 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "George", last_name: "Washington", hourly_rate: 60)
+@store1.employees.create(first_name: "John", last_name: "Adams", hourly_rate: 60)
+@store1.employees.create(first_name: "Thomas", last_name: "Jefferson", hourly_rate: 60)
+@store1.employees.create(first_name: "James", last_name: "Madison", hourly_rate: 60)
+@store1.employees.create(first_name: "James", last_name: "Monroe", hourly_rate: 60)
+
+@store2.employees.create(first_name: "John", last_name: "Macdonald", hourly_rate: 60)
+@store2.employees.create(first_name: "Alexander", last_name: "Mackenzie", hourly_rate: 60)
+@store2.employees.create(first_name: "John", last_name: "Abbott", hourly_rate: 60)
+@store2.employees.create(first_name: "John", last_name: "Thompson", hourly_rate: 60)
+@store2.employees.create(first_name: "Mackenzie", last_name: "Bowell", hourly_rate: 60)
+
+puts Employee.count
