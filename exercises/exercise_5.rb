c8b7d84ab49c0499ce357041bf3d1e94cebd2eb8
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-01-04 04:02:16 -0500

    solve exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,8 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts "Total company annual revenue: $#{Store.sum("annual_revenue")}"
+puts "Average store annual revenue: $#{Store.average("annual_revenue").to_i}"
+
+@high_sales_stores = Store.where(["annual_revenue > 1000000"])
+puts "Number of stores generating $1M+ in annual sales: #{@high_sales_stores.count}"
