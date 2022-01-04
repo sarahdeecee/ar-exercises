64faf08a1d527aaad3ded2653026c2bfc2d5690d
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-01-04 04:26:06 -0500

    solve exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,13 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Enter a new store name:"
+new_store_name = gets.chomp
+new_store = Store.create(name: new_store_name)
+
+if new_store.errors
+  puts "Could not create store. See errors:"
+  new_store.errors.full_messages.each do |error|
+    puts "- #{error}"
+  end
+end
