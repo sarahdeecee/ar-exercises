c287a77b184f5473a0d33ace0e18162133eadada
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-01-04 15:58:50 -0500

    solve bonus exercise 7

--- lib/store.rb
+++ lib/store.rb
@@ -2,4 +2,10 @@ class Store < ActiveRecord::Base
   has_many :employees
   validates :name, length: { minimum: 3 }
   validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :sells_mens_or_womens_apparel
+
+  def sells_mens_or_womens_apparel
+    errors.add(:mens_apparel, "or Womens apparel must be true") unless (mens_apparel || womens_apparel)
+  end
+
 end
